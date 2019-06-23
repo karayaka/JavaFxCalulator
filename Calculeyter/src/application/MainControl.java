e6425f2ed52adff49a4ee28640dbdc230f45a823
çağrı KARAYAKA <cagri.karayaka@gmail.com>
2019-06-23 23:14:26 +0300

    Java Fx ile MVC yöntemine uygun hesapmakinası

--- Calculeyter/src/application/MainControl.java
+++ Calculeyter/src/application/MainControl.java
@@ -1,20 +1,54 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class MainControl {
+	@FXML
+	private Label sonuc;
+	private long nuber1=0;
+	private String operator="";
+	private boolean start=true;
+	private Model model=new Model();	
+	
 
-	public MainControl() {
-		// TODO Auto-generated constructor stub
+	public MainControl() {		
 		
 		
 	}
 	public void processNumbers(ActionEvent evet) {
+		if(start) {
+			sonuc.setText("");
+			start=false;
+			
+		}
+		String deger=((Button)evet.getSource()).getText(); 
+		sonuc.setText(sonuc.getText()+deger);
 		
 		
 	}
 	
 	public void processOperators(ActionEvent evet) {
+		String value=((Button)evet.getSource()).getText();
+		if(!value.equals("=")) {
+			if(!operator.isEmpty()) {
+				return;
+			}
+			operator=value;
+			nuber1=Long.parseLong(sonuc.getText());
+			sonuc.setText("");
+			
+		}else {
+			if(operator.isEmpty()) {
+				return;
+			}
+			long number2=Long.parseLong(sonuc.getText());
+			float output= model.calculate(nuber1, number2, operator);
+			sonuc.setText(String.valueOf(output));
+		}
+		
 		
 		
 	}
--- Calculeyter/src/application/Model.java
+++ Calculeyter/src/application/Model.java
@@ -9,8 +9,27 @@ public class Model {
 	}
 	public float calculate(long nuber1,long nuber2,String operator) {
 		
+		switch (operator) {
+		case "+":
+			
+			return nuber1+nuber2;
+		case "-":
+			
+			return nuber1-nuber2;
+		case "*":
+			
+			return nuber1*nuber2;
+		case "/":
+			if(nuber2==0) {
+				return 0;
+			}
+			return nuber1/nuber2;
+
+		default:
+			return 0;
+		}
 		
-		return 0;
+		
 	}
 
 }
