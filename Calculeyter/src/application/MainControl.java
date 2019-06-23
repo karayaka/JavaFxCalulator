4a10abe1c1fd37c371dadb8452720aca17f55c38
çağrı KARAYAKA <cagri.karayaka@gmail.com>
2019-06-23 20:13:13 +0300

    First Fx Project

---
+++ Calculeyter/src/application/MainControl.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class MainControl {
+
+	public MainControl() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+	public void processNumbers(ActionEvent evet) {
+		
+		
+	}
+	
+	public void processOperators(ActionEvent evet) {
+		
+		
+	}
+
+}
---
+++ Calculeyter/src/application/Model.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Model {
+	
+	
+
+	public Model() {
+		// TODO Auto-generated constructor stub
+	}
+	public float calculate(long nuber1,long nuber2,String operator) {
+		
+		
+		return 0;
+	}
+
+}
